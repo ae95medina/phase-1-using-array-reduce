81a8ba72bc599450b03e02ce86710c865916769f
ae95medina <ae95medina@gmail.com>
2022-05-12 10:03:34 -0500

    commit tree 3f88bf89b7a860753934e02636f0e5c94e640af9

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// let totalBatteries = 0
+// batteries = batteryBatches.reduce(function(acc,arr){
+//     return  acc + arr; }, totalBatteries);
 
-// Code your solution here
+//     const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// totalBatteries = batteryBatches.reduce(function(acc,arr){
+//     return  acc + arr; }, 0);
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = batteryBatches.reduce(function(acc,arr){
+    return  acc + arr; }, 0);
